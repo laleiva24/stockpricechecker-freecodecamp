2a58d58b7b4b2068d5c4b0065099f63711225db7
Ismael Alejandro Leiva Mendoza <ismael.leiva24@gmail.com>
2023-07-15 11:00:47 -0600

    first commit

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,98 @@
 'use strict';
+const StockModel = require("../models").Stock;
+const fetch = require('node-fetch');
+
+async function createStock(stock, like, ip) {
+  const newStock = new StockModel({
+    symbol: stock,
+    likes: like ? [ip] : [],
+  });
+  const savedNew = await newStock.save();
+  return savedNew;
+}
+
+async function findStock(stock) {
+  return await StockModel.findOne({ symbol: stock }).exec();
+}
+
+async function saveStock(stock, like, ip) {
+  let saved = {};
+  const foundStock = await findStock(stock);
+  if (!foundStock) {
+    const createsaved = await createStock(stock, like, ip);
+    saved = createsaved;
+    return saved;
+  } else {
+    if (like && foundStock.likes.indexOf(ip) === -1) {
+      foundStock.likes.push(ip);
+    }
+    saved = await foundStock.save();
+    return saved;
+  }
+}
+
+async function getStock(stock) {
+  const response = await fetch(
+    `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`
+  );
+  const { symbol, latestPrice } = await response.json();
+  return { symbol, latestPrice };
+}
 
 module.exports = function (app) {
+  app.route('/api/stock-prices').get(async function (req, res) {
+    const { stock, like } = req.query;
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    if (typeof stock === 'string') {
+      const { symbol, latestPrice } = await getStock(stock);
+
+      if (!symbol) {
+        res.json({ stockData: { likes: like ? 1 : 0 } });
+        return;
+      }
+
+      const stockData = await saveStock(symbol, like, req.ip);
+
+      res.json({
+        stockData: {
+          stock: symbol,
+          price: latestPrice,
+          likes: stockData.likes.length,
+        },
+      });
+    } else if (Array.isArray(stock) && stock.length === 2) {
+      const [stock1, stock2] = stock;
+      const [data1, data2] = await Promise.all([
+        getStock(stock1),
+        getStock(stock2),
+      ]);
+
+      if (!data1.symbol || !data2.symbol) {
+        res.json({ error: 'One or both stocks are not found' });
+        return;
+      }
+
+      const [stockData1, stockData2] = await Promise.all([
+        saveStock(data1.symbol, like, req.ip),
+        saveStock(data2.symbol, like, req.ip),
+      ]);
+
+      res.json({
+        stockData: [
+          {
+            stock: data1.symbol,
+            price: data1.latestPrice,
+            rel_likes: stockData1.likes.length - stockData2.likes.length,
+          },
+          {
+            stock: data2.symbol,
+            price: data2.latestPrice,
+            rel_likes: stockData2.likes.length - stockData1.likes.length,
+          },
+        ],
+      });
+    } else {
+      res.json({ error: 'Invalid request' });
+    }
+  });
 };
--- server.js
+++ server.js
@@ -1,34 +1,46 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+require('./db-connection');
 
+let helmet = require('helmet');
 const app = express();
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"]
+  }
+}));
+
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -36,15 +48,16 @@ app.use(function(req, res, next) {
     .send('Not Found');
 });
 
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+
+// Start our server and tests
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
@@ -52,4 +65,5 @@ const listener = app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for testing
+module.exports = app; // for testing
+
